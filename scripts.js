7b4a3fc1e635c5840c2d4a0f29657f604bd34128
Pedro Madruga <pedro.madruga@liferay.com>
2026-04-06 20:32:27 -0300

    NOISSUE Add section-me styling | script.js file

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Access Pedro Madruga github's
+const portfolioBtn = document.querySelector("section.me button")
+
+const goToGithub = () => {
+    const GITHUB_USER_URL = 'https://github.com/madruga0';
+    window.open(GITHUB_USER_URL);
+}
